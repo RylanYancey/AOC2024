795af358ecd9cb2fbc23e390a470a8548fe18ed3
RylanYancey <rylanyancey@gmail.com>
2024-12-02 18:19:20 -0500

    AOC 2

---
+++ src/aoc2.rs
@@ -0,0 +1,82 @@
+
+use anyhow::Result;
+
+pub fn aoc2_1() -> Result<()> {
+    let src = std::fs::read_to_string("input/aoc2.txt")?;
+
+    let mut nums = Vec::new();
+    let mut safe = 0;
+
+    for line in src.lines() {
+        // collect numbers into a buffer
+        nums.clear();
+        line.split(" ").map(|n| n.parse::<i32>().unwrap()).collect_into(&mut nums);        
+
+        if !report_is_safe(&nums) {
+            continue;
+        }
+
+        safe += 1;
+    }
+
+    println!("(AOC 2_1) Number of safe reports: {safe}");
+
+    Ok(())
+}
+
+pub fn aoc2_2() -> Result<()> {
+    let src = std::fs::read_to_string("input/aoc2.txt")?;
+
+    let mut nums = Vec::new();
+    let mut safe = 0;
+
+    for line in src.lines() {
+        // collect numbers into a buffer
+        nums.clear();
+        line.split(" ").map(|n| n.parse::<i32>().unwrap()).collect_into(&mut nums);      
+
+        // if the report can be made safe by
+        // removing a number, it still counts as safe.
+        if !report_is_safe(&nums) {
+            let mut without1 = Vec::new();
+
+            'inner:
+            for i in 0..nums.len() {
+                nums.clone_into(&mut without1);
+                without1.remove(i);
+
+                if report_is_safe(&without1) {
+                    safe += 1;
+                    break 'inner;
+                }
+            }
+        } else {
+            safe += 1;
+        }
+    }
+    println!("(AOC 2_2) Number of safe reports: {safe}");
+
+    Ok(())
+}
+
+fn report_is_safe(nums: &[i32]) -> bool {
+    // determine if there are any invalid adjacentcies.
+    for i in 1..nums.len()-1 {
+        let d1 = (nums[i] - nums[i-1]).abs();
+        let d2 = (nums[i] - nums[i+1]).abs();
+
+        if !(1..=3).contains(&d1) || !(1..=3).contains(&d2) {
+            return false;
+        }
+    }
+
+    // determine if all numbers are ascending or descending.
+    let dir = (nums[0] - nums[1]).signum();
+    for i in 0..nums.len()-1 {
+        if (nums[i] - nums[i+1]).signum() != dir {
+            return false;
+        }
+    }
+
+    true
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 
+#![feature(iter_collect_into)]
+
 mod aoc1;
+mod aoc2;
 
 fn main() {
     // aoc1::aoc1_1().unwrap();
-    aoc1::aoc1_2().unwrap();
+    // aoc1::aoc1_2().unwrap();
+    // aoc2::aoc2_1().unwrap();
+    aoc2::aoc2_2().unwrap();
 }
